c48b65f2f988e3161d68f30b3ea05aea85081d69
Mikel Hensley <mikel.i.hensley@gmail.com>
2018-07-10 22:18:49 -0400

    Styled the main nav, added jQuery to show and hide sections via the navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,32 @@
 
+
 $('#nav-header').hide().fadeIn(1000);
 
 $('.speaker-item').hide().fadeIn(2000);
+
+$('.section').hide();
+
+$('#info').show();
+
+$('#speaker-link').click( function() {
+  $('.section').hide();
+  $('#speakers').show();
+});
+
+$('#info-link').click( function() {
+  $('.section').hide();
+  $('#info').show();
+});
+
+$('#venue-link').click( function() {
+  $('.section').hide();
+  $('#venue').show();
+});
+
+$('#schedule-link').click( function() {
+  $('.section').hide();
+  $('#schedule').show();
+});
+
+
+
